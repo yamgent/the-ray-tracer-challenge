f1f983f708c28cd38bf1784b6834cc7a7b61b741
Maurice Tan Wang Leng <tanwangleng@outlook.com>
2023-05-10 21:16:19 +0800

    Add basic sphere intersection

--- src/geometry.rs
+++ src/geometry.rs
@@ -23,6 +23,37 @@ impl Ray {
     }
 }
 
+pub struct Sphere {
+    origin: Point3f,
+}
+
+impl Sphere {
+    pub fn new() -> Self {
+        Self {
+            origin: Point3f::new(0.0, 0.0, 0.0),
+        }
+    }
+
+    pub fn intersect_ray(&self, ray: &Ray) -> Vec<f64> {
+        let sphere_to_ray = ray.origin - self.origin;
+
+        let a = ray.direction.dot(&ray.direction);
+        let b = 2.0 * ray.direction.dot(&sphere_to_ray);
+        let c = sphere_to_ray.dot(&sphere_to_ray) - 1.0;
+
+        let discriminant = (b * b) - (4.0 * a * c);
+
+        if discriminant < 0.0 {
+            vec![]
+        } else {
+            vec![
+                (-b - discriminant.sqrt()) / (2.0 * a),
+                (-b + discriminant.sqrt()) / (2.0 * a),
+            ]
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -43,5 +74,27 @@ mod tests {
         assert_eq!(r.position(1.0), Point3f::new(3.0, 3.0, 4.0));
         assert_eq!(r.position(-1.0), Point3f::new(1.0, 3.0, 4.0));
         assert_eq!(r.position(2.5), Point3f::new(4.5, 3.0, 4.0));
+    }
+
+    #[test]
+    fn test_sphere_intersect() {
+        [
+            ((0.0, 0.0, -5.0), vec![4.0, 6.0]),
+            ((0.0, 1.0, -5.0), vec![5.0, 5.0]),
+            ((0.0, 2.0, -5.0), vec![]),
+            ((0.0, 0.0, 0.0), vec![-1.0, 1.0]),
+            ((0.0, 0.0, 5.0), vec![-6.0, -4.0]),
+        ]
+        .into_iter()
+        .for_each(|(starting_point, expected)| {
+            let r = Ray::new(
+                Point3f::new(starting_point.0, starting_point.1, starting_point.2),
+                Vector3f::new(0.0, 0.0, 1.0),
+            );
+            let s = Sphere::new();
+
+            let result = s.intersect_ray(&r);
+            assert_eq!(result, expected);
+        });
     }
 }
