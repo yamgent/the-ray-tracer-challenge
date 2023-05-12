4db42d3fcd1f3217c91d32cae80e4fae9cd2f966
Maurice Tan Wang Leng <tanwangleng@outlook.com>
2023-05-12 22:16:39 +0800

    Add result image

---
+++ README.md
@@ -0,0 +1,5 @@
+# the-ray-tacer-challenge
+
+## Chapter 05
+
+![ch05_circle](./results/ch05_circle.png)
--- src/bin/ch05_circle/main.rs
+++ src/bin/ch05_circle/main.rs
@@ -13,8 +13,9 @@ fn draw_scenario(
     canvas: &mut Canvas,
     canvas_start_x: usize,
     canvas_start_y: usize,
+    wall_color: Color,
     sphere: Sphere,
-    color: Color,
+    sphere_color: Color,
 ) {
     let wall_size = 7.0;
     let wall_z = 10.0;
@@ -31,9 +32,15 @@ fn draw_scenario(
             let dest = Point3f::new(d_x, d_y, wall_z);
 
             let ray = Ray::new(camera, dest - camera);
-            if ray.intersect_sphere(&sphere).hit().is_some() {
-                canvas.write_px(canvas_start_x + x, canvas_start_y + y, color);
-            }
+            canvas.write_px(
+                canvas_start_x + x,
+                canvas_start_y + y,
+                if ray.intersect_sphere(&sphere).hit().is_some() {
+                    sphere_color
+                } else {
+                    wall_color
+                },
+            );
         });
     });
 }
@@ -41,10 +48,13 @@ fn draw_scenario(
 fn main() {
     let mut canvas = Canvas::new(PER_DRAWING_SIZE * 3, PER_DRAWING_SIZE * 2);
 
+    let wall_colors = [Color::new(0.7, 0.7, 0.7), Color::new(0.8, 0.8, 0.8)];
+
     draw_scenario(
         &mut canvas,
         0,
         0,
+        wall_colors[0],
         Sphere::default(),
         Color::new(1.0, 0.0, 0.0),
     );
@@ -53,6 +63,7 @@ fn main() {
         &mut canvas,
         PER_DRAWING_SIZE,
         0,
+        wall_colors[1],
         Sphere::new(Matrix4x4f::scaling(Vector3f::new(1.0, 0.5, 1.0))),
         Color::new(0.0, 1.0, 0.0),
     );
@@ -61,6 +72,7 @@ fn main() {
         &mut canvas,
         PER_DRAWING_SIZE * 2,
         0,
+        wall_colors[0],
         Sphere::new(Matrix4x4f::scaling(Vector3f::new(0.5, 1.0, 1.0))),
         Color::new(0.0, 0.0, 1.0),
     );
@@ -69,6 +81,7 @@ fn main() {
         &mut canvas,
         0,
         PER_DRAWING_SIZE,
+        wall_colors[1],
         Sphere::new(
             Matrix4x4f::identity()
                 .scale(Vector3f::new(0.5, 1.0, 1.0))
@@ -81,6 +94,7 @@ fn main() {
         &mut canvas,
         PER_DRAWING_SIZE,
         PER_DRAWING_SIZE,
+        wall_colors[0],
         Sphere::new(
             Matrix4x4f::identity()
                 .scale(Vector3f::new(0.5, 1.0, 1.0))
