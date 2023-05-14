8eb2f9cb2f57ddbcff47652f37a944046445b989
Maurice Tan Wang Leng <tanwangleng@outlook.com>
2023-05-14 14:36:44 +0800

    Add lighting function

--- src/graphics.rs
+++ src/graphics.rs
@@ -8,7 +8,9 @@ pub struct Color {
 }
 
 impl Color {
-    pub fn new(r: f64, g: f64, b: f64) -> Self {
+    pub const BLACK: Self = Color::new(0.0, 0.0, 0.0);
+
+    pub const fn new(r: f64, g: f64, b: f64) -> Self {
         Self { vals: [r, g, b] }
     }
 
--- src/shading.rs
+++ src/shading.rs
@@ -1,4 +1,7 @@
-use crate::{graphics::Color, math::Point3f};
+use crate::{
+    graphics::Color,
+    math::{Point3f, Vector3f},
+};
 
 pub struct PointLight {
     pub position: Point3f,
@@ -49,8 +52,45 @@ impl Default for Material {
     }
 }
 
+pub struct LightingArgs {
+    material: Material,
+    light: PointLight,
+    point: Point3f,
+    eyev: Vector3f,
+    normalv: Vector3f,
+}
+
+pub fn lighting(args: LightingArgs) -> Color {
+    let effective_color = args.material.color * args.light.intensity;
+    let lightv = (args.light.position - args.point).normalize();
+    let ambient = effective_color * args.material.ambient;
+
+    let light_dot_normal = lightv.dot(&args.normalv);
+
+    let (diffuse, specular) = if light_dot_normal < 0.0 {
+        // light is on the other side
+        (Color::BLACK, Color::BLACK)
+    } else {
+        let diffuse = effective_color * args.material.diffuse * light_dot_normal;
+        let reflectv = (-lightv).reflect(&args.normalv);
+        let reflect_dot_eye = reflectv.dot(&args.eyev);
+
+        let specular = if reflect_dot_eye <= 0.0 {
+            Color::BLACK
+        } else {
+            let factor = reflect_dot_eye.powf(args.material.shininess);
+            args.light.intensity * args.material.specular * factor
+        };
+
+        (diffuse, specular)
+    };
+
+    ambient + diffuse + specular
+}
+
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
@@ -65,6 +105,68 @@ mod tests {
         assert_eq!(
             Material::default(),
             Material::new(Color::new(1.0, 1.0, 1.0), 0.1, 0.9, 0.9, 200.0)
+        );
+    }
+
+    #[test]
+    fn test_lighting() {
+        let material = Material::default();
+        let position = Point3f::new(0.0, 0.0, 0.0);
+        let normalv = Vector3f::new(0.0, 0.0, -1.0);
+
+        assert_eq!(
+            lighting(LightingArgs {
+                eyev: Vector3f::new(0.0, 0.0, -1.0),
+                normalv,
+                light: PointLight::new(Point3f::new(0.0, 0.0, -10.0), Color::new(1.0, 1.0, 1.0)),
+                material,
+                point: position,
+            }),
+            Color::new(1.9, 1.9, 1.9)
+        );
+
+        assert_eq!(
+            lighting(LightingArgs {
+                eyev: Vector3f::new(0.0, 2_f64.sqrt() / 2.0, -2_f64.sqrt() / 2.0),
+                normalv,
+                light: PointLight::new(Point3f::new(0.0, 0.0, -10.0), Color::new(1.0, 1.0, 1.0)),
+                material,
+                point: position,
+            }),
+            Color::new(1.0, 1.0, 1.0)
+        );
+
+        assert_eq!(
+            lighting(LightingArgs {
+                eyev: Vector3f::new(0.0, 0.0, -1.0),
+                normalv,
+                light: PointLight::new(Point3f::new(0.0, 10.0, -10.0), Color::new(1.0, 1.0, 1.0)),
+                material,
+                point: position,
+            }),
+            Color::new(0.7363961030678927, 0.7363961030678927, 0.7363961030678927)
+        );
+
+        assert_eq!(
+            lighting(LightingArgs {
+                eyev: Vector3f::new(0.0, -2_f64.sqrt() / 2.0, -2_f64.sqrt() / 2.0),
+                normalv,
+                light: PointLight::new(Point3f::new(0.0, 10.0, -10.0), Color::new(1.0, 1.0, 1.0)),
+                material,
+                point: position,
+            }),
+            Color::new(1.6363961030678928, 1.6363961030678928, 1.6363961030678928)
+        );
+
+        assert_eq!(
+            lighting(LightingArgs {
+                eyev: Vector3f::new(0.0, 0.0, -1.0),
+                normalv,
+                light: PointLight::new(Point3f::new(0.0, 0.0, 10.0), Color::new(1.0, 1.0, 1.0)),
+                material,
+                point: position,
+            }),
+            Color::new(0.1, 0.1, 0.1)
         );
     }
 }
